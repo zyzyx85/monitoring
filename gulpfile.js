02433f470a44c02b741ce8ae9444ad281419c1b0
Federico Gallucci <federico.gallucci@spindox.it>
2014-11-11 21:19:11 +0100

    Refactoring gulpfile!

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 /**
  * Created by federico on 06/11/14.
  */
-var gulp = require('gulp')
+var gulp = require('gulp');
 var concat = require("gulp-concat");
 var less = require("gulp-less");
 var autoprefixer = require('gulp-autoprefixer');
-var browserify = require('browserify')
-var source = require('vinyl-source-stream')
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var minifyHtml = require("gulp-minify-html");
 
 gulp.task('build-app', function () {
   return browserify('./client/assets/js/init.js')
@@ -23,15 +24,18 @@ gulp.task('build-less', function () {
     .pipe(gulp.dest('./www/css'));
 });
 
-
-//gulp.task('default', ['serve']);
-//gulp.task('default', ['build-app','build-less']);
+gulp.task('minify-html', function () {
+  gulp.src("./client/html/index.html")
+    .pipe(minifyHtml())
+    .pipe(gulp.dest("./www"));
+});
 
 gulp.task('default', [
   'build-less',
-  'build-app'
+  'build-app',
+  'minify-html'
 ], function () {
   gulp.watch('./client/assets/css/*.less', ['build-less']);
-  gulp.watch('index.html', ['minify-html']);
+  gulp.watch('./client/index.html', ['minify-html']);
   gulp.watch('./client/assets/js/**/*.js', ['build-app']);
 });
